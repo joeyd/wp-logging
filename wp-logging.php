be718eaca7a2c025896a28a20f03cdb4fa956628
JoeyD <williamdurham@gmail.com>
2014-05-14 13:50:45 -0400

    Update

--- wp-logging.php
+++ wp-logging.php
@@ -9,25 +9,6 @@ Author URI: http://www.nologyinteractive.com/
 License: GPL2
 */
 
-if ( is_admin() ) {
-  function nolo_plugin_get_version() {
-    if ( ! function_exists( 'get_plugins' ) )
-        require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-    $nolojd_plugin_data = get_plugin_data( __FILE__ );
-    return $nolojd_plugin_data['Version'];
-  }
-
-  add_action('init', 'nolo_activate_auto_update');
-  function nolo_activate_auto_update()
-  {
-      require_once ('includes/nolo-class-auto-update.php');
-      $nolojd_plugin_current_version = nolo_plugin_get_version();
-      $nolojd_plugin_remote_path = 'http://www.nologyinteractive.com/nolo-repo/?p='.basename(__FILE__, '.php');
-      $nolojd_plugin_slug = plugin_basename(__FILE__);
-      new nolojd_auto_update ($nolojd_plugin_current_version, $nolojd_plugin_remote_path, $nolojd_plugin_slug);
-  }
-}
-
 // apache_request_headers() is not available for everyone so we use a function
 if( !function_exists('apache_request_headers') ) {
   function apache_request_headers() {
